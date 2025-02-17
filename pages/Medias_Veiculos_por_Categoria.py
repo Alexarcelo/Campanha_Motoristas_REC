cf3c6792de8ef135f288dd64134932272ca2875f
Alexarcelo <marcelo@luckreceptivo.com.br>
2025-02-17 17:55:10 -0300

    Update Medias_Veiculos_por_Categoria.py

--- pages/Medias_Veiculos_por_Categoria.py
+++ pages/Medias_Veiculos_por_Categoria.py
@@ -24,29 +24,45 @@ with row0[0]:
 
 if 'df_servicos_abastecimentos' in st.session_state:
 
+    df_servicos_abastecimentos = st.session_state.df_servicos_abastecimentos.copy()
+
     if data_inicial and data_final:
+
+        df_servicos_abastecimentos = df_servicos_abastecimentos[(df_servicos_abastecimentos['Data da Escala']>=data_inicial) & (df_servicos_abastecimentos['Data da Escala']<=data_final)]
 
         with row0[0]:
 
-            veiculo = container_datas.multiselect('Veículo', sorted(st.session_state.df_servicos_abastecimentos['Veiculo'].unique()), default=None)
+            tipo_veiculo = container_datas.multiselect('Tipo de Veículo', sorted(df_servicos_abastecimentos['Tipo de Veículo'].unique()), default=None)
+
+            if len(tipo_veiculo)>0:
+
+                df_servicos_abastecimentos = df_servicos_abastecimentos[df_servicos_abastecimentos['Tipo de Veículo'].isin(tipo_veiculo)]
+
+            veiculo = container_datas.multiselect('Veículo', sorted(df_servicos_abastecimentos['Veiculo'].unique()), default=None)
+
+            if len(veiculo)>0:
+
+                df_servicos_abastecimentos = df_servicos_abastecimentos[df_servicos_abastecimentos['Veiculo'].isin(veiculo)]
 
         with row1[0]:
 
             st.divider()
 
-        if len(veiculo)>0:
+        st.header('Resultados por Tipo de Veículo')
 
-            df_veiculos = st.session_state.df_servicos_abastecimentos[(st.session_state.df_servicos_abastecimentos['Veiculo'].isin(veiculo)) & 
-                                                                      (st.session_state.df_servicos_abastecimentos['Data da Escala']>=data_inicial) & 
-                                                                      (st.session_state.df_servicos_abastecimentos['Data da Escala']<=data_final)]
+        df_media_tp_veiculos = df_servicos_abastecimentos.groupby(['Tipo de Veículo', 'Categoria Meta'])[['Litros', 'Km Rodado']].sum().reset_index()
 
-            df_media_categoria = df_veiculos.groupby(['Veiculo', 'Categoria Meta'])[['Litros', 'Km Rodado']].sum().reset_index()
+        df_media_tp_veiculos['Média Km/Litro'] = round(df_media_tp_veiculos['Km Rodado'] / df_media_tp_veiculos['Litros'], 2)
 
-            df_media_categoria['Média Km/Litro'] = round(df_media_categoria['Km Rodado'] / df_media_categoria['Litros'], 2)
+        st.dataframe(df_media_tp_veiculos, hide_index=True, use_container_width=True)
 
-            container_dataframe = st.container()
+        st.header('Resultados por Veículo')
 
-            container_dataframe.dataframe(df_media_categoria, hide_index=True, use_container_width=True)
+        df_media_veiculos = df_servicos_abastecimentos.groupby(['Veiculo', 'Tipo de Veículo', 'Categoria Meta'])[['Litros', 'Km Rodado']].sum().reset_index()
+
+        df_media_veiculos['Média Km/Litro'] = round(df_media_veiculos['Km Rodado'] / df_media_veiculos['Litros'], 2)
+
+        st.dataframe(df_media_veiculos, hide_index=True, use_container_width=True)
     
 else:
 
