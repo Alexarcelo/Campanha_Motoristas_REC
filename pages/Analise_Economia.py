6ce9e0a0380096d63d7b27c12852d18008fc27af
Alexarcelo <marcelo@luckreceptivo.com.br>
2025-02-18 14:23:59 -0300

    Update Analise_Economia.py

--- pages/Analise_Economia.py
+++ pages/Analise_Economia.py
@@ -2,20 +2,25 @@ import streamlit as st
 import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder
 
-def gerar_df_grid(df, colunas_rename, colunas_df_grid):
-
-    df['Valor/Litro'] = df['Valor Total'] / df['Litros']
-
-    df['Valor Meta'] = df['Litros Consumidos Meta'] * df['Valor/Litro']
-
-    df['Economia'] = df.apply(lambda row: f"Economia de R${round(row['Valor Meta'] - row['Valor Total'], 2)}" if row['Valor Meta']>=row['Valor Total'] else 
-                              f"Prejuízo de R${round(row['Valor Total'] - row['Valor Meta'], 2)}", axis=1)
+def criar_df_merge(df_resumo_performance_tipo_veiculo, df_resumo_performance_tipo_veiculo_base, coluna_merge):
+        
+    df_resumo_performance_tipo_veiculo['Km/l | Período Atual'] = round(df_resumo_performance_tipo_veiculo['Km Rodado'] / df_resumo_performance_tipo_veiculo['Litros'], 2)
     
-    df = df.rename(columns=colunas_rename)
-
-    df_geral_colunas = df[colunas_df_grid]
-
-    return df_geral_colunas
+    df_resumo_performance_tipo_veiculo_base['Km/l | Período Base'] = round(df_resumo_performance_tipo_veiculo_base['Km Rodado'] / df_resumo_performance_tipo_veiculo_base['Litros'], 2)
+    
+    df_resumo_performance_tipo_veiculo_geral = pd.merge(df_resumo_performance_tipo_veiculo, df_resumo_performance_tipo_veiculo_base, on=coluna_merge, how='left')
+    
+    df_resumo_performance_tipo_veiculo_geral['Economia em Litros'] = round((df_resumo_performance_tipo_veiculo_geral['Km Rodado_x'] / 
+                                                                            df_resumo_performance_tipo_veiculo_geral['Km/l | Período Base']) - df_resumo_performance_tipo_veiculo_geral['Litros_x'], 0)
+    
+    df_resumo_performance_tipo_veiculo_geral['Valor Litro'] = round(df_resumo_performance_tipo_veiculo_geral['Valor Total'] / df_resumo_performance_tipo_veiculo_geral['Litros_x'], 2)
+    
+    df_resumo_performance_tipo_veiculo_geral['Economia em R$'] = df_resumo_performance_tipo_veiculo_geral['Economia em Litros'] * df_resumo_performance_tipo_veiculo_geral['Valor Litro']
+    
+    df_resumo_performance_tipo_veiculo_geral_colunas = df_resumo_performance_tipo_veiculo_geral[[coluna_merge, 'Km/l | Período Base', 'Km/l | Período Atual', 'Economia em Litros', 'Valor Litro', 
+                                                                                                 'Economia em R$']]
+    
+    return df_resumo_performance_tipo_veiculo_geral_colunas
 
 st.set_page_config(layout='wide')
 
@@ -27,15 +32,23 @@ row1 = st.columns(2)
 
 row2 = st.columns(1)
 
-row3 = st.columns(2)
+row3 = st.columns(1)
 
 with row1[0]:
 
-    st.subheader('Período:')
+    st.subheader('Comparar período de:')
 
     data_inicial = st.date_input('Data Inicial', value=None, format='DD/MM/YYYY', key='data_inicial')
 
     data_final = st.date_input('Data Final', value=None, format='DD/MM/YYYY', key='data_final')
+
+with row1[1]:
+
+    st.subheader('Em relação à:')
+
+    data_inicial_base = st.date_input('Data Inicial', value=None, format='DD/MM/YYYY', key='data_inicial_base')
+
+    data_final_base = st.date_input('Data Final', value=None, format='DD/MM/YYYY', key='data_final_base')
 
 if data_inicial and data_final:
 
@@ -49,12 +62,13 @@ if data_inicial and data_final:
     
     df_filtro_data = df_abastecimentos[(df_abastecimentos['Apenas Data']>=data_inicial) & (df_abastecimentos['Apenas Data']<=data_final) & (df_abastecimentos['Km/Litro']>0)].reset_index(drop=True)
 
-    df_filtro_data['Litros Consumidos Meta'] = df_filtro_data.apply(lambda row: round(row['Km Rodado']/row['Meta'], 2), axis=1)
+    df_base = df_abastecimentos[(df_abastecimentos['Apenas Data']>=data_inicial_base) & (df_abastecimentos['Apenas Data']<=data_final_base) & (df_abastecimentos['Km/Litro']>0)].reset_index(drop=True)
 
-    df_resumo_performance_tipo_veiculo = df_filtro_data.groupby('Tipo de Veículo').agg({'Km Rodado': 'sum', 'Litros': 'sum', 'Litros Consumidos Meta': 'sum', 'Valor Total': 'sum'}).reset_index()
+    df_resumo_performance_tipo_veiculo = df_filtro_data.groupby('Tipo de Veículo').agg({'Km Rodado': 'sum', 'Litros': 'sum', 'Valor Total': 'sum'}).reset_index()
+    
+    df_resumo_performance_tipo_veiculo_base = df_base.groupby('Tipo de Veículo').agg({'Km Rodado': 'sum', 'Litros': 'sum'}).reset_index()
 
-    df_resumo_performance_tipo_veiculo_geral_colunas = gerar_df_grid(df_resumo_performance_tipo_veiculo, {'Litros': 'Litros Consumidos Real'}, 
-                                                                     ['Tipo de Veículo', 'Km Rodado', 'Litros Consumidos Real', 'Litros Consumidos Meta', 'Economia'])
+    df_resumo_performance_tipo_veiculo_geral_colunas = criar_df_merge(df_resumo_performance_tipo_veiculo, df_resumo_performance_tipo_veiculo_base, 'Tipo de Veículo')
 
     gb = GridOptionsBuilder.from_dataframe(df_resumo_performance_tipo_veiculo_geral_colunas)
     gb.configure_selection('single')
@@ -63,7 +77,8 @@ if data_inicial and data_final:
 
     with row3[0]:
 
-        grid_response = AgGrid(df_resumo_performance_tipo_veiculo_geral_colunas, gridOptions=gridOptions, enable_enterprise_modules=False, fit_columns_on_grid_load=True)
+        grid_response = AgGrid(df_resumo_performance_tipo_veiculo_geral_colunas, gridOptions=gridOptions, 
+                                enable_enterprise_modules=False, fit_columns_on_grid_load=True)
 
     selected_rows = grid_response['selected_rows']
 
@@ -74,31 +89,40 @@ if data_inicial and data_final:
         if tipo_veiculo:
 
             df_resumo_performance_veiculo = df_filtro_data[df_filtro_data['Tipo de Veículo']==tipo_veiculo].groupby('Veiculo')\
-                .agg({'Km Rodado': 'sum', 'Litros': 'sum', 'Litros Consumidos Meta': 'sum', 'Valor Total': 'sum'}).reset_index()
+                .agg({'Km Rodado': 'sum', 'Litros': 'sum', 'Valor Total': 'sum'}).reset_index()
             
-            df_resumo_performance_veiculo_geral_colunas = gerar_df_grid(df_resumo_performance_veiculo, {'Litros': 'Litros Consumidos Real', 'Veiculo': 'Veículo'}, 
-                                                                        ['Veículo', 'Km Rodado', 'Litros Consumidos Real', 'Litros Consumidos Meta', 'Economia'])
+            df_resumo_performance_veiculo_base = df_base[df_base['Tipo de Veículo']==tipo_veiculo].groupby('Veiculo')\
+                .agg({'Km Rodado': 'sum', 'Litros': 'sum'}).reset_index()
+            
+            df_resumo_performance_veiculo_geral_colunas = criar_df_merge(df_resumo_performance_veiculo, df_resumo_performance_veiculo_base, 
+                                                                        'Veiculo')
             
             gb = GridOptionsBuilder.from_dataframe(df_resumo_performance_veiculo_geral_colunas)
             gb.configure_selection('single')
             gb.configure_grid_options(domLayout='autoHeight')
             gridOptions = gb.build()
 
-            with row3[1]:
+            with row3[0]:
 
-                grid_response = AgGrid(df_resumo_performance_veiculo_geral_colunas, gridOptions=gridOptions, enable_enterprise_modules=False, fit_columns_on_grid_load=True)
+                grid_response = AgGrid(df_resumo_performance_veiculo_geral_colunas, gridOptions=gridOptions, 
+                                       enable_enterprise_modules=False, fit_columns_on_grid_load=True)
                 
             selected_rows_2 = grid_response['selected_rows']
 
             if selected_rows_2 is not None and len(selected_rows_2)>0:
 
-                veiculo = selected_rows_2['Veículo'].iloc[0]
+                veiculo = selected_rows_2['Veiculo'].iloc[0]
                 
-                df_resumo_performance_motorista_veiculo = df_filtro_data[(df_filtro_data['Veiculo']==veiculo) & (df_filtro_data['Tipo de Veículo']==tipo_veiculo)].groupby('Motorista')\
-                    .agg({'Km Rodado': 'sum', 'Litros': 'sum', 'Litros Consumidos Meta': 'sum', 'Valor Total': 'sum'}).reset_index()
+                df_resumo_performance_motorista_veiculo = df_filtro_data[(df_filtro_data['Veiculo']==veiculo) & 
+                                                                         (df_filtro_data['Tipo de Veículo']==tipo_veiculo)].groupby('Colaborador')\
+                    .agg({'Km Rodado': 'sum', 'Litros': 'sum', 'Valor Total': 'sum'}).reset_index()
                 
-                df_resumo_performance_motorista_veiculo_geral_colunas = gerar_df_grid(df_resumo_performance_motorista_veiculo, {'Litros': 'Litros Consumidos Real'}, 
-                                                                                      ['Motorista', 'Km Rodado', 'Litros Consumidos Real', 'Litros Consumidos Meta', 'Economia'])
+                df_resumo_performance_motorista_veiculo_base = df_base[(df_base['Veiculo']==veiculo) & 
+                                                                              (df_base['Tipo de Veículo']==tipo_veiculo)].groupby('Colaborador')\
+                    .agg({'Km Rodado': 'sum', 'Litros': 'sum'}).reset_index()
+                
+                df_resumo_performance_motorista_veiculo_geral_colunas = criar_df_merge(df_resumo_performance_motorista_veiculo, 
+                                                                                       df_resumo_performance_motorista_veiculo_base, 'Colaborador')
 
                 gb = GridOptionsBuilder.from_dataframe(df_resumo_performance_motorista_veiculo_geral_colunas)
                 gb.configure_selection('single')
@@ -107,4 +131,5 @@ if data_inicial and data_final:
 
                 with row3[1]:
 
-                    grid_response = AgGrid(df_resumo_performance_motorista_veiculo_geral_colunas, gridOptions=gridOptions, enable_enterprise_modules=False, fit_columns_on_grid_load=True)
+                    grid_response = AgGrid(df_resumo_performance_motorista_veiculo_geral_colunas, gridOptions=gridOptions, enable_enterprise_modules=False, 
+                                            fit_columns_on_grid_load=True)
